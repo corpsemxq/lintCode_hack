13ab4416f839febf07526c8f9851fb8a698703d6
Richard Mao <richard.mao@crowdnetic.com>
2019-10-30 15:48:15 -0400

    add travis for composer

--- .travis.sh
+++ .travis.sh
@@ -9,9 +9,14 @@ php --version
   cd $(mktemp -d)
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 )
-composer install
+# composer install
 
-# php composer.phar require hhvm/hsl hhvm/hhvm-autoload
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+php composer.phar require hhvm/hsl hhvm/hhvm-autoload
 
 hh_client
 vendor/bin/hacktest tests/
